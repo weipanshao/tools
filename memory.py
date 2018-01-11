7a9ef0a9933d87878a21f73551e90f7dc1b44d5a
weipanshao <842025688@qq.com>
2018-01-11 22:51:50 +0800

    Update memory.py

--- memory.py
+++ memory.py
@@ -2,15 +2,19 @@ import sys
 
 file = open("memory.txt")
 total = 0
+total_used = 0
 while 1:
     line = file.readline()
     if not line:
         break
     size = int(line.split()[2])
+    used = int(line.split()[4])
     num = int(line.split()[8])
-    
-    #print "Size:{} num:{} total_mem:{}"%(size)%(num)%(size*num)
-    print size,num,size*num
-    total += size*num
 
-print total
+    #print "Size:{} num:{} total_mem:{}"%(size)%(num)%(size*num)
+    print size,num,size*used,size*num
+    total += size*num
+    total_used += size*used
+    
+
+print total_used,total
